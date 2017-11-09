2e9fe9bdcc4939fc10d9cdc5331b5765a3fbb202
vandonley <vandonley@gmail.com>
2017-11-09 09:56:04 -0800

    Quick script for when RMM doesn't detect Windows Backup correctly.

---
+++ Windows-Backup-Check.ps1
@@ -0,0 +1,22 @@
+$after = [datetime]::Now.AddDays(-1)
+$logs = Get-WinEvent Microsoft-Windows-Backup | Where { $_.TimeCreated -ge $after }
+if (!($logs)) {
+Write-Host "No backup events found!"
+Exit 1001
+}
+$errors = $logs | Where-Object { $_.LevelDisplayName -ne "Information" }
+if ($errors) {
+Write-Host "Backup errors found!"
+Write-Host "--------------------------"
+$errors | Format-List | Out-String
+Exit 1001
+}
+$success = $logs | Where-Object { ($_.id -eq 4) -and ($_.Message -eq "The backup operation has finished successfully.") }
+if (!($success)) {
+Write-Host "No successfull backup found!"
+Exit 1001
+}
+else {
+Write-Host "Backup Success!"
+Exit 0
+}
