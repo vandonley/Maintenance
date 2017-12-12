19927a1bf2c4995b9ab7d83f7a56cd15369ddcc7
vandonley <vandonley@gmail.com>
2017-12-12 10:36:21 -0800

    Cleaned up output

--- vitClean-Disk.ps1
+++ vitClean-Disk.ps1
@@ -1,63 +1,147 @@
 ﻿<#
---------------------------
-Automate running Disk Cleanup with all
-options enabled.
-Parsec Computer Corp.
-Created:  Van Donley - 04/14/2017
-Last Updated:  Van Donely - 04/17/2017
---------------------------
+.Synopsis
+   Runs Windows Disk Cleanup with all available options
+.DESCRIPTION
+   The script is to be uploaded to your dashboard account as a user script.
+   It can run both as a script check and as a scheduled task. Expects to be run
+   with vitCheck-RMMDefaults.ps1
+   
+.EXAMPLE
+   .\vitClean-Disk.ps1
+.OUTPUTS
+   Error file
+.EMAIL
+   vdonley@visionit.net
+.VERSION
+   1.0
 #>
 
-function pccClean-Disk
-{
+# We are only binding -logfile.
+param (	
+	# Make sure -logfile is NOT positional
+	[Parameter(Mandatory=$false)]
+    [string]$logfile
+)
 
-# Create hashtable for return from function
+# Set error and warning preferences
+$ErrorActionPreference = "Continue"
+$WarningPreference = "Continue"
 
-[hashtable]$Return = @{}
+# Force output to keep RMM from timing out
+Write-Host ' '
 
-# Registry information to configure Disk Cleanup
+# Create hashtable for output. Make it stay in order and start an error counter to create an alert if needed. Divide by 1 to force integer
+$Return = [ordered]@{}
+$Return.Error_Count = 0
 
-$strKeyPath   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches"
-$strValueName = "StateFlags0042"
-$subkeys      = Get-ChildItem -Path $strKeyPath -Name
-
-# Try setting the registry keys and catch any errors
-
-    try
-    { 
-        ForEach($subkey in $subkeys){
-            $null = New-ItemProperty -Path $strKeyPath\$subkey -Name $strValueName -PropertyType DWord -Value 2 }
-
-       $Return.AddRegKeys = "Success"
+# REGION Reporting setup
+try {
+    # Information about the script for reporting.
+    $ErrorFileName = "vitClean-Disk.txt"
+    # File name for ScriptRunnter
+    $Return.RMM_Script_Name = $MyInvocation.MyCommand.Name
+    # Check to see if the RMM Error Folder exists. Put the Error file in %TEMP% if it doesn't.
+    $myErrorPath = $env:RMMErrorFolder
+    if ($myErrorPath) {
+        $Return.Error_File = $env:RMMErrorFolder + "\" + $ErrorFileName
     }
-    
-    catch [Exception] { $Return.AddRegKeys = $_.Exception.Message | Format-List | Out-String }
-
-# Try running Disk Cleanup
-    
-    try
-    {
-        Start-Process cleanmgr.exe -ArgumentList "/sagerun:42" -Wait -NoNewWindow
-
-        $Return.RunCleanup = "Running cleanmgr.exe"
+    else {
+        $Return.Error_File = $env:TEMP + "\" + $ErrorFileName
     }
-    
-    catch [Exception] { $Return.RunCleanup = $_.Exception.Message | Format-List | Out-String }
-
-# Try removing the registry keys
-    
-    try
-    {
-        ForEach($subkey in $subkeys){
-            $null = Remove-ItemProperty -Path $strKeyPath\$subkey -Name $strValueName }
-        
-        $Return.DelRegKeys = "Success" 
-    }
-
-    catch [Exception] { $Return.DelRegKeys = $_.Exception.Message | Format-List | Out-String }
-
- return $Return
-
 }
+catch {
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.File_Information_Catch = $myException 
+    $Return.Error_Count++ 
+}
+# END REGION
 
-pccClean-Disk    
+# REGION Prerequisites
+try {
+    # Path to Disk Cleanup
+    $DiskCleanPath = $env:windir + '\system32\cleanmgr.exe'
+    # Make sure Disk Cleanup is installed, exit if it is not
+    if (Test-Path $DiskCleanPath) {
+        $Return.Disk_Cleanup_Found = $DiskCleanPath
+    }
+    else {
+        $Return.Disk_Cleanup_Found = "Error - Disk Cleanup not found, install or remove task"
+        Write-Output @"
+    
+Script could not run!
+Troubleshooting info below
+_______________________________
+    
+"@
+        $Return | Format-List | Out-String
+        Exit 0
+    }
+}
+catch {
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.Prerequisite_Catch = $myException 
+    $Return.Error_Count++ 
+}
+# END REGION
+
+# REGION Run Disk Cleanup
+try
+    {
+    # Registry key information for Disk Cleanup
+    $strKeyPath   = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Explorer\VolumeCaches"
+    $strValueName = "StateFlags0042"
+    $subkeys      = Get-ChildItem -Path $strKeyPath -Name
+    
+    # Set all options to run in registry
+    ForEach($subkey in $subkeys) {
+        $null = New-ItemProperty -Path $strKeyPath\$subkey -Name $strValueName -PropertyType DWord -Value 2
+    }
+    $Return.Registry_Keys_Update = "Success"
+    
+    # Run Disk Cleanup
+    $Return.Disk_Cleanup_Result = Start-Process -FilePath $DiskCleanPath -ArgumentList "/sagerun:42" -Wait -NoNewWindow | Out-String
+    
+    #Remove the registry keys
+    ForEach($subkey in $subkeys) {
+        $null = Remove-ItemProperty -Path $strKeyPath\$subkey -Name $strValueName
+    }
+        $Return.Registry_Keys_Delete = "Success"
+}
+catch { 
+    $myException = $_.Exception | Format-List | Out-String
+    $Return.Disk_Cleanup_Catch = $myException 
+    $Return.Error_Count++
+}
+# END REGION
+
+# REGION Output results and create an alert if needed
+if ($Return.Error_Count -eq 0) {
+    Write-Output @"
+    
+Script Success!
+Troubleshooting info below
+_______________________________
+   
+"@
+    $Return | Format-List | Out-String
+    if (Test-Path $Return.Error_File) {
+        Remove-Item $Return.Error_File
+    }
+    Exit 0
+    }
+else {
+    Write-Output @"
+    
+Script Failure!
+Troubleshooting info below
+_______________________________
+
+"@
+    $Return | Format-List | Out-String
+    Add-Content -Path $Return.Error_File -Value "`n----------------------`n "
+	Add-Content -Path $Return.Error_File -Value (get-date) -passthru
+	Add-Content -Path $Return.Error_File -Value "`n "
+	Add-Content -Path $Return.Error_File -Value ( $Return | Format-List | Out-String )
+    Exit 1001
+}
+# END REGION
